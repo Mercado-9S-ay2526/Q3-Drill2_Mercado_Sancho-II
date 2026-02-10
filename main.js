6d414d2cdd4bfa4a0cd429526d8e80f5e8490adb
Mercado-9S-ay2526 <mercadosancholl@gmail.com>
2026-02-10 08:38:22 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function CircleArea() {
+    var radius = document.getElementById("radius").value;
+    var area = Math.PI * radius * radius();
+    document.getElementById("answer").area.toFixed(2);
+}
